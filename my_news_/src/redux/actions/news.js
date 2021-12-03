292e0990c8d05fc07f12b810d714edf33ddd06de
Dasha0507 <93385654+Dasha0507@users.noreply.github.com>
2021-12-03 18:03:38 +0300

    Add files via upload

---
+++ my_news_/src/redux/actions/news.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export const fetchNews =  (subscription, category) => (dispatch) => {
+    axios.get(`http://localhost:3010/news?${subscription === 1 ? 
+    `subscription=0&subscription=1` : `subscription=0`}&${
+        category !== null ? `category=${category}` : `category=0&category=1&category=2&category=3`
+    }`)
+    .then(({data}) => {
+           dispatch(setNews(data));
+    });
+};
+
+
+export const setNews = (items) => ({
+    type: 'SET_NEWS',
+    payload: items,
+});
+
